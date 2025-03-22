bde556bf3f9189d7ee95b1bda4a7934317274417
Martyna Szpiech <martyna.szpiech@op.pl>
2025-03-22 22:24:28 +0100

    capitalize inital letters

--- task_1.py
+++ task_1.py
@@ -4,3 +4,9 @@ shopping_list = {
 }
 for shop, products in shopping_list.items():
     print(f"Idę do {shop} i kupuję tam {products}")
+
+capitalized_letters = {shop.capitalize(): [product.capitalize() for product in products]
+                        for shop, products in shopping_list.items()}
+print(capitalized_letters)
+for shop, products in capitalized_letters.items():
+    print(f"{shop}: {products}")
